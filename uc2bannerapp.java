22a1fc298a86d36a642c6509213ac47f6723fe6b
Amulya <amulyathimmareddy@gmail.com>
2026-03-17 02:39:42 +0530

    uc2 done

---
+++ uc2bannerapp.java
@@ -0,0 +1,25 @@
+public class uc2bannerapp {
+    public static void main(String[] args) {
+
+        System.out.println(String.join("   ",
+                " ***** ", " ***** ", " ******", " ***** "));
+
+        System.out.println(String.join("   ",
+                "*     *", "*     *", "*     *", "*     "));
+
+        System.out.println(String.join("   ",
+                "*     *", "*     *", "*     *", "*     "));
+
+        System.out.println(String.join("   ",
+                "*     *", "*     *", " ******", " ***** "));
+
+        System.out.println(String.join("   ",
+                "*     *", "*     *", "*      ", "      *"));
+
+        System.out.println(String.join("   ",
+                "*     *", "*     *", "*      ", "      *"));
+
+        System.out.println(String.join("   ",
+                " ***** ", " ***** ", "*      ", " ***** "));
+    }
+}

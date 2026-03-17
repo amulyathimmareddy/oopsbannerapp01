0f6cf735ca0400cf16348e78dc7cf076b8d0b536
Amulya <amulyathimmareddy@gmail.com>
2026-03-17 17:14:01 +0530

    uc04 done

---
+++ uc4bannerapp.java
@@ -0,0 +1,20 @@
+public class uc4bannerapp {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+            " *****     *****     ******     ***** ",
+            "*     *   *     *   *     *   *     ",
+            "*     *   *     *   *     *   *     ",
+            "*     *   *     *    ******     ***** ",
+            "*     *   *     *   *                *",
+            "*     *   *     *   *                *",
+            " *****     *****    *           ***** "
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+
+}

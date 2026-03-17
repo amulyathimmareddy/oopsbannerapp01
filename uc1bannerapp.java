606353b7cba488752c34f20d90b2810e19f8320b
Amulya <amulyathimmareddy@gmail.com>
2026-03-17 13:44:25 +0530

    uc01 done

--- uc1bannerapp.java
+++ uc1bannerapp.java
@@ -1,15 +1,7 @@
 public class uc1bannerapp {
-    
     public static void main(String[] args) {
 
-        System.out.println(" *****    *****    *****    ***** ");
-        System.out.println("*     *  *     *  *     *  *      ");
-        System.out.println("*     *  *     *  *     *  *      ");
-        System.out.println("*     *  *     *  *****    *****  ");
-        System.out.println("*     *  *     *  *             * ");
-        System.out.println("*     *  *     *  *             * ");
-        System.out.println(" *****    *****    *       *****  ");
+        System.out.println("OOPS");
 
     }
-
 }
